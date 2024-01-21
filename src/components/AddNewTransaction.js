9965c61e2e0eee1e7e8b36e8a14ae4ba4b590be3
unknown <omersim15185@gmail.com>
2024-01-21 23:24:03 +0200

    add the amount of product to the list

--- src/components/AddNewTransaction.js
+++ src/components/AddNewTransaction.js
@@ -4,33 +4,41 @@ import TransactionList from './TransactionList';
 
 export default function AddNewTransaction() {
   const [addLabel, setAddLabel] = useState(0);
+  const [amounts, setAmounts] = useState([]);
+ 
+
+  const handleAmountChange = (e, index) => {
+    const updatedAmounts = [...amounts];
+    updatedAmounts[index] = { amount: e.target.value };
+    setAmounts(updatedAmounts);
+  };
 
   const handleAddLabel = () => {
-    // Increment the component count on button click
     setAddLabel(addLabel + 1);
+    setAmounts([...amounts, { amount: '' }]);
   };
- 
+
   return (
-    <div className='AddNewTransaction'> 
-    {Array.from({ length: addLabel }).map((_, index) => (
-        <TransactionList key={index} showTitle={false} />
+    <div className='AddNewTransaction'>
+      {Array.from({ length: addLabel }).map((_, index) => (
+        <TransactionList key={index} showTitle={false} amount={amounts[index]?.amount || ''}  />
       ))}
       <h3>Add new transaction</h3>
       <div className='Line-New-Transaction'></div>
       <form id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label><br></br>
-          
-          <input type="text" id="text" placeholder="Enter text..."  />
-          
+
+          <input type="text" id="text" placeholder="Enter text..." />
+
         </div>
 
         <div className="form-control">
           <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
-          
-          <input type="number" id="amount" placeholder="Enter amount..."  />
+
+          <input type="number" id="amount"  value={amounts[addLabel]?.amount || ''}
+            onChange={(e) => handleAmountChange(e, addLabel)} placeholder="Enter amount..." />
         </div>
-        
         <button type="button" className="btn" onClick={handleAddLabel} >
           Add transaction
         </button>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
 
 
-export default function TransactionList({showTitle}) {
+export default function TransactionList({showTitle = true , amount }) {
 
 
-  
+  console.log({amount});
   return (
     <div>
        {showTitle && <h3>History</h3>}
         <ul id="list" className="list" >
           <li className="minus">
-            Cash <span>-$400</span><button className="delete-btn">x</button>
+            Cash  <span>-${amount}</span><button className="delete-btn">x</button>
           </li>
         </ul>
    
